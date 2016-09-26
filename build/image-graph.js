24a1f04374a4cd9e2677f6a2763faf408a92cdfc
Jean-Sébastien Perreault <jean-sebperro@outlook.com>
2016-09-26 12:58:51 -0400

    Integration of images into the chart

--- build/image-graph.js
+++ build/image-graph.js
@@ -40,7 +40,13 @@ System.register("image-graph", ["angular2/core"], function($__export) {
             })).nice();
             svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis).append("text").attr("class", "label").attr("x", width).attr("y", -6).style("text-anchor", "end").text("Sepal Width (cm)");
             svg.append("g").attr("class", "y axis").call(yAxis).append("text").attr("class", "label").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text("Sepal Length (cm)");
-            svg.selectAll(".dot").append("svg:image").attr("xlink:href", "http://www.clker.com/cliparts/1/4/5/a/1331068897296558865Sitting%20Racoon.svg").attr("width", 200).attr("height", 200);
+            svg.selectAll(".dot").data(data).enter().append("svg:image").attr('width', 20).attr('height', 24).attr("x", function(d) {
+              return x(d.sepalWidth);
+            }).attr("y", function(d) {
+              return y(d.sepalLength);
+            }).attr("xlink:href", function(d) {
+              return d.image;
+            });
             var legend = svg.selectAll(".legend").data(color.domain()).enter().append("g").attr("class", "legend").attr("transform", function(d, i) {
               return "translate(0," + i * 20 + ")";
             });
